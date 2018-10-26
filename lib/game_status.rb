f9268352fe669dbe0995ec57fa3e88221756612c
Elias Ventura <eliasventura23@gmail.com>
2018-10-26 12:46:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,6 +9,6 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6, 7, 8], [0,3,6], [1,4,7], [2,5,8], [0,4
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    combo
+    board[combo[0]] == board[combo[1]]
   end
 end
